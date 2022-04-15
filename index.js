e9947853cb4c3f5346c80bcfb96dfa6170c9e6d6
josueJURE <josue.jure@gmail.com>
2022-04-15 10:33:20 +0100

    fixed error thrown by console

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ console.log(images[counter])
 
 
 nextBtn.addEventListener("click", function() {
-  if(counter <= images.length -1) {
+  if(counter < images.length) {
     images.forEach(image => {
       image.classList.add("hide");
     })
-    images[counter].classList.remove("hide");
+    if(counter === -1) {
+      counter = counter + 1;
+      images[counter].classList.remove("hide");
+    } else {
+      images[counter].classList.remove("hide");
+    }
     counter++;
+    counter -1
     console.log(counter)
   }
 })
@@ -21,8 +27,12 @@ prevBtn.addEventListener("click", function() {
   if(counter < 0) return
   images.forEach(image => {
     image.classList.add("hide");
-    console.log(counter)
-    images[counter].classList.remove("hide");
+
+    if(counter === 3) {
+      counter = counter -1
+      images[counter].classList.remove("hide")
+    } else {
+      images[counter].classList.remove("hide");
+    }
   })
-  counter--;
 })
