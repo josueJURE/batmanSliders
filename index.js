854ca6e3222af9595b5b3813c5f336ab3dc1c53c
josueJURE <josue.jure@gmail.com>
2022-04-16 08:45:03 +0100

    create batman object

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const nextBtn = document.getElementById("nextBtn");
 const images = Array.from(document.querySelectorAll(".carousel_Image"));
 let counter = 0;
 
+let batman = [
+  {
+    name: "affleck",
+    year: 2013,
+    boxOffice: "$738.4 million"
+  },
+  {
+    name: "keaton",
+    year: 1989,
+    boxOffice: "$266.9 million"
+  },
+  {
+    name: "pattinson",
+    year: 2022,
+    boxOffice: "$238.5 million"
+  }
+]
+
+console.log(batman[counter].name)
+
 nextBtn.addEventListener("click", moveNext);
 prevBtn.addEventListener("click", moveToPrevious);
 
